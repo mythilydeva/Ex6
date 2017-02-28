ba65fc77f550fff0a504dc88f5f0adab96ff01b7
ITLAB <ITLAB@IT212>
2017-02-28 14:31:41 +0530

    commit2

--- src/com/example/multithreading/MainActivity.java
+++ src/com/example/multithreading/MainActivity.java
@@ -1,35 +1,47 @@
 package com.example.multithreading;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+public class MainActivity extends Activity {
+	private ProgressBar bar;
 
-public class MainActivity extends ActionBarActivity {
+	/** Called when the activity is first created. */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		bar = (ProgressBar) findViewById(R.id.progressBar1);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public void startProgress(View view) {
+
+		bar.setProgress(0);
+		new Thread(new Task()).start();
+	}
+
+	class Task implements Runnable {
+		@Override
+		public void run() {
+			for (int i = 0; i <= 10; i++) {
+				final int value = i;
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				bar.setProgress(value);
+
+			}
+		}
+
+	}
+
 }
